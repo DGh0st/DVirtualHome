629b73a97d88751aee137ce5b6204f5a5d82324d
Deep Patel <deeppwnage@yahoo.com>
2017-05-02 18:06:35 -0500

    changed back UndoRotation to DVirtualHome

--- dvirtualhome/DVHRootListController.h
+++ dvirtualhome/DVHRootListController.h
@@ -2,12 +2,12 @@
 #import <Preferences/PSTableCell.h>
 #import <MessageUI/MFMailComposeViewController.h>
 
-@interface PSTableCell (UndoRotation)
+@interface PSTableCell (DVirtualHome)
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier;
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier;
 @end
 
-@interface PSListController (UndoRotation)
+@interface PSListController (DVirtualHome)
 - (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion;
 - (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion;
 - (UINavigationController*)navigationController;
